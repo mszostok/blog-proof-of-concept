9feed2283dc5714aaa3639e3a03b3e8789aa0d74
Mateusz Szostok <szostok.mateusz@gmail.com>
2016-05-23 17:27:46 +0200

    Add tomcat dependecies, init proper web app configuration

--- src/main/java/com/mszostok/BlogApplication.java
+++ src/main/java/com/mszostok/BlogApplication.java
@@ -1,10 +1,19 @@
 package com.mszostok;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class BlogApplication {
+@ComponentScan
+@EnableAutoConfiguration
+public class BlogApplication  extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
