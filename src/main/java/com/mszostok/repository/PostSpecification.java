783f2fd61c370fc075578d92b3e1e1efa2d9e29d
Mateusz Szostok <szostok.mateusz@gmail.com>
2016-05-26 14:30:52 +0200

    Add post specification to get post by give date, with focus only on year and month

---
+++ src/main/java/com/mszostok/repository/PostSpecification.java
@@ -0,0 +1,44 @@
+package com.mszostok.repository;
+
+import com.mszostok.domain.Post;
+import org.springframework.data.jpa.domain.Specification;
+
+
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Defines a specification as a predicate over an Post which return all post for given
+ * date, with focus only on year and month
+ * @author mszostok
+ */
+public class PostSpecification implements Specification<Post> {
+
+    private final Date date;
+
+    public PostSpecification(Date date) {
+        this.date = date;
+    }
+    @Override
+    public Predicate toPredicate(Root<Post> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (date != null) {
+            Calendar dateCalendar = Calendar.getInstance();
+            dateCalendar.setTime(date);
+
+            Path<Date> rootPostDate = root.get("postDate");
+            return cb.and(
+                    cb.equal(cb.function("year", Integer.class, rootPostDate), dateCalendar.get(Calendar.YEAR)),
+                    cb.equal(cb.function("month", Integer.class, rootPostDate), dateCalendar.get(Calendar.MONTH) + 1)
+            );
+
+        }
+        return null;
+    }
+
+
+}
